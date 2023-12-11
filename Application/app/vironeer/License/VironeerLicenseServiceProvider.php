dac74d9f344c93a2e1fcacc2084660cb6859af28
Densuke Alpha <Kitretsusaisama@protonmail.com>
2023-12-11 12:37:51 +0530

    update

--- Application/app/vironeer/License/VironeerLicenseServiceProvider.php
+++ Application/app/vironeer/License/VironeerLicenseServiceProvider.php
@@ -6,15 +6,15 @@ use App\Http\Kernel;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
-use Vironeer\License\App\Http\Middleware\DemoTypeMiddleware;
-use Vironeer\License\App\Http\Middleware\InstalledMiddleware;
-use Vironeer\License\App\Http\Middleware\NoSaasMiddleware;
-use Vironeer\License\App\Http\Middleware\NotInstalledMiddleware;
-use Vironeer\License\App\Http\Middleware\SaasMiddleware;
+use App\Vironeer\License\App\Http\Middleware\DemoTypeMiddleware;
+use App\Vironeer\License\App\Http\Middleware\InstalledMiddleware;
+use App\Vironeer\License\App\Http\Middleware\NoSaasMiddleware;
+use App\Vironeer\License\App\Http\Middleware\NotInstalledMiddleware;
+use App\Vironeer\License\App\Http\Middleware\SaasMiddleware;
 
 class VironeerLicenseServiceProvider extends ServiceProvider
 {
-    protected $namespace = 'Vironeer\License\App\Http\Controllers';
+    protected $namespace = 'App\Vironeer\License\App\Http\Controllers';
 
     public function boot()
     {
