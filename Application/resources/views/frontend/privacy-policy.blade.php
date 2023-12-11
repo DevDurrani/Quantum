acedcbfc871634893b0ee934a1c6fc847d6aabe9
Densuke Alpha <Kitretsusaisama@protonmail.com>
2023-12-12 00:12:06 +0530

    update

--- Application/resources/views/frontend/privacy-policy.blade.php
+++ Application/resources/views/frontend/privacy-policy.blade.php
@@ -8,9 +8,7 @@
             <p>At <b>QuantumSafe</b>, accessible from <b>https://qsafe.hustleandstyle.com </b>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by <b>QuantumSafe</b> and how we use it.</p>
 
             <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us at email <b>hntlsquad@gmail.com</b>.</p>
-
-            <p>This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in <b>QuantumSafe</b>. This policy is not applicable to any information collected offline or via channels other than this website.Our Privacy Policy was created with the help of the <a href="https://raptorkit.com/privacy-policy-generator">RaptorKit Privacy Policy Generator</a>.</p></p>
-
+            
             <h2>Consent</h2>
 
             <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
--- Application/resources/views/frontend/term-of-use.blade.php
+++ Application/resources/views/frontend/term-of-use.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="section-content col-lg-7 m-auto">
 
-            <h1>Terms and Conditions for <b>QuantumSafe</b></h1>
+            <h1>Terms and Conditions for</h1>
 
             <p> Welcome to <b>QuantumSafe</b> </p>
             <p> These terms and conditions outline the rules and regulations for the use of <b>QuantumSafe</b>, located at <b>https://qsafe.hustleandstyle.com</b>.</p>
@@ -28,8 +28,6 @@
                 <li>Reproduce, duplicate or copy material from <b>QuantumSafe</b></li>
                 <li>Redistribute content from <b>QuantumSafe</b></li>
             </ul>
-
-            <p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href="https://raptorkit.com/terms-and-conditions-generator">Free Terms and Conditions Generator</a>.</p>
 
             <p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. <b>QuantumSafe</b> does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of <b>QuantumSafe</b>,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, <b>QuantumSafe</b> shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
             <p><b>QuantumSafe</b> reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
