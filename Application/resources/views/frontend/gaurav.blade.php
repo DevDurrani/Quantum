fe28cd61a24ddfd231794eed0dfe73d3b3068f0e
Densuke Alpha <Kitretsusaisama@protonmail.com>
2023-12-22 00:31:53 +0530

    update

--- Application/resources/views/frontend/gaurav.blade.php
+++ Application/resources/views/frontend/gaurav.blade.php
@@ -4,101 +4,17 @@
 @section('content')
     <div class="container">
         <div class="section-content col-lg-7 m-auto">
-            <h1>Gaurav Gupta</h1>
-
-            <p>At <b>QuantumSafe</b>, accessible from <b>https://qsafe.hustleandstyle.com </b>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by <b>QuantumSafe</b> and how we use it.</p>
-
-            <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us at email <b>hntlsquad@gmail.com</b>.</p>
-
-            <h2>Consent</h2>
-
-            <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
-
-            <h2>Information we collect</h2>
-
-            <p>The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.</p>
-            <p>If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
-            <p>When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
-
-            <h2>How we use your information</h2>
-
-            <p>We use the information we collect in various ways, including to:</p>
-
-            <ul>
-                <li>Provide, operate, and maintain our website</li>
-                <li>Improve, personalize, and expand our website</li>
-                <li>Understand and analyze how you use our website</li>
-                <li>Develop new products, services, features, and functionality</li>
-                <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
-                <li>Send you emails</li>
-                <li>Find and prevent fraud</li>
-            </ul>
-
-            <h2>Log Files</h2>
-
-            <p><b>QuantumSafe</b> follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.</p>
-
-            <h2>Cookies and Web Beacons</h2>
-
-            <p>Like any other website, tempse uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.</p>
-
-            <h2>Google DoubleClick DART Cookie</h2>
-
-            <p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads" target="_blank" rel="noopener">https://policies.google.com/technologies/ads</a></p>
-
-
-
-            <h2>Our Advertising Partners</h2>
-
-            <p>Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below. Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier access, we hyperlinked to their Privacy Policies below.</p>
-
-            <ul>
-                <li>
-                    <p>Google</p>
-                    <p><a href="https://policies.google.com/technologies/ads" target="_blank" rel="noopener">https://policies.google.com/technologies/ads</a></p>
-                </li>
-            </ul>
-
-            <h2>Advertising Partners Privacy Policies</h2>
-
-            <P>You may consult this list to find the Privacy Policy for each of the advertising partners of <b>QuantumSafe</b>.</p>
-
-            <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on <b>QuantumSafe</b>, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
-
-            <p>Note that <b>QuantumSafe</b> has no access to or control over these cookies that are used by third-party advertisers.</p>
-
-            <h2>Third Party Privacy Policies</h2>
-
-            <p><b>QuantumSafe</b>'s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. </p>
-
-            <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
-
-            <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
-
-            <p>Under the CCPA, among other rights, California consumers have the right to:</p>
-            <p>Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
-            <p>Request that a business delete any personal data about the consumer that a business has collected.</p>
-            <p>Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
-            <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
-
-            <h2>GDPR Data Protection Rights</h2>
-
-            <p>We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:</p>
-            <p>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</p>
-            <p>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</p>
-            <p>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</p>
-            <p>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</p>
-            <p>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</p>
-            <p>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</p>
-            <p>If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
-
-            <h2>Children's Information</h2>
-
-            <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
-
-            <p><b>QuantumSafe</b> does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
-
+            <p><strong> Gurav Gupta - Programming Virtuoso and Docker Maestro</strong></p>
+            <p>👨&zwj;💻 <strong>About Me:</strong> Hello world! I'm Gurav Gupta, a passionate and innovative software developer hailing from the vibrant world of programming. With a heart pulsating in binary and a mind that orchestrates code, I thrive on the thrill of creating seamless digital experiences.</p>
+            <p>🚀 <strong>Programming Prowess:</strong> My journey in the realm of coding began with the humble "Hello, World!" and has since evolved into a symphony of languages. Proficient in languages such as Python, Java, and JavaScript, I take pride in crafting elegant and efficient solutions to complex problems. My code isn't just functional; it's a work of art.</p>
+            <p>🐳 <strong>Docker Dynamo:</strong> A true Docker aficionado, I excel in containerization and orchestration. Docker is not just a tool for me; it's a canvas on which I paint scalable, portable, and consistent applications. From building containerized microservices to orchestrating them with Kubernetes, I navigate the Docker ecosystem with finesse.</p>
+            <p>🔧 <strong>Toolbox Expertise:</strong> My toolkit extends beyond coding languages and Docker. I am well-versed in a plethora of development tools, ensuring that I can choose the right tool for the job. Whether it's version control with Git, continuous integration with Jenkins, or deployment with Ansible, I believe in harnessing the power of the best tools available.</p>
+            <p>🌐 <strong>Web Wizardry:</strong> Front-end or back-end, I've got both realms covered. From crafting sleek and intuitive user interfaces using HTML, CSS, and JavaScript to architecting robust server-side applications, I ensure that every line of code contributes to a seamless digital experience.</p>
+            <p>📚 <strong>Lifelong Learner:</strong> In the ever-evolving landscape of technology, I embrace the ethos of continuous learning. Staying updated with the latest trends, frameworks, and best practices is not just a habit; it's a commitment to delivering cutting-edge solutions.</p>
+            <p>🌟 <strong>Community Contributor:</strong> Beyond my individual journey, I am an active contributor to the developer community. Whether it's sharing insights on blogs, participating in open-source projects, or mentoring budding developers, I believe in giving back to the community that has shaped me.</p>
+            <p>💡 <strong>Innovation Advocate:</strong> Innovation is not just a buzzword for me; it's a way of life. I thrive on pushing the boundaries of what's possible, experimenting with new technologies, and architecting solutions that not only meet requirements but also spark excitement.</p>
+            <p>🌈 <strong>Outside the IDE:</strong> When not immersed in code, you'll find me exploring the world of sci-fi literature, brewing the perfect cup of coffee, or challenging myself with outdoor adventures. Life is a balance, and I believe in savoring every moment.</p>
+            <p>Let's code the future together! 🚀✨</p>
         </div>
-    </div>
     </div>
 @endsection
